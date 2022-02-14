c065e71ffeecd8730e888e410f9241fc4b99e04c
ArcherGu <649601376@qq.com>
2022-02-14 15:38:30 +0800

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import WindiCSS from 'vite-plugin-windicss';
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import Components from 'unplugin-vue-components/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,5 +25,31 @@ export default defineConfig({
             ]
         }),
         Icons({ compiler: 'vue3' }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: '不上茶屋',
+                short_name: '不上茶屋',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/pwa-250x250.png',
+                        sizes: '250x250',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-300x300.png',
+                        sizes: '300x300',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-300x300.png',
+                        sizes: '300x300',
+                        type: 'image/png',
+                        purpose: 'maskable any',
+                    },
+                ],
+            },
+        }),
     ]
 })
