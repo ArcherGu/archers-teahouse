77c578531ac0f9409ec57d4a17c66aab2b9e5f0c
ArcherGu <649601376@qq.com>
2022-04-10 13:58:40 +0800

    chore: downgrade vue-tsc

--- src/components/Tea/Tea.vue
+++ src/components/Tea/Tea.vue
@@ -10,47 +10,53 @@ import { makeStep } from '@/store';
 
 const canvasWrapper = ref(null);
 let tea: Tea;
-let lastSize = "0 + 0";
+let lastSize = '0 + 0';
 
 const onResize = () => {
-    const wrapper = unrefElement(canvasWrapper) as HTMLElement;
+    const wrapper = unrefElement(canvasWrapper);
     if (!wrapper) return;
     const { clientHeight: height, clientWidth: width } = wrapper;
     const currentSize = `${width} + ${height}`;
     if (lastSize === currentSize) return;
 
     lastSize = currentSize;
-    tea?.resize(width, height)
-}
+    tea?.resize(width, height);
+};
 
 const toImage = () => {
     return tea?.toImage();
-}
+};
 
-watch(canvasWrapper, () => {
-    const wrapper = unrefElement(canvasWrapper) as HTMLElement;
-    if (!wrapper) return;
-    tea = new Tea(wrapper);
-    tea.init();
-    window.addEventListener('resize', onResize, false);
-}, {
-    flush: 'post',
-});
+watch(
+    canvasWrapper,
+    () => {
+        const wrapper = unrefElement(canvasWrapper);
+        if (!wrapper) return;
+        tea = new Tea(wrapper);
+        tea.init();
+        window.addEventListener('resize', onResize, false);
+    },
+    {
+        flush: 'post',
+    }
+);
 
-watch(() => makeStep.value, () => {
-    setTimeout(() => {
-        onResize()
-    }, 500);
-})
+watch(
+    () => makeStep.value,
+    () => {
+        setTimeout(() => {
+            onResize();
+        }, 500);
+    }
+);
 
 onUnmounted(() => {
-    window.removeEventListener('resize', onResize, false)
-})
+    window.removeEventListener('resize', onResize, false);
+});
 
 defineExpose({
-    toImage
-})
+    toImage,
+});
 </script>
 
-<style>
-</style>
+<style></style>
